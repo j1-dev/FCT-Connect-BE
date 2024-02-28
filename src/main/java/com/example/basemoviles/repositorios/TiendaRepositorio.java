d5792e7847fcc1051cf40f857c680738240ca456
j1-dev <aephyxen@gmail.com>
2024-02-28 10:18:59 +0100

    Limpieza

--- src/main/java/com/example/basemoviles/repositorios/TiendaRepositorio.java
+++ src/main/java/com/example/basemoviles/repositorios/TiendaRepositorio.java
@@ -1,7 +1,5 @@
 package com.example.basemoviles.repositorios;
 
-import com.example.basemoviles.entidades.Tienda;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface TiendaRepositorio extends CrudRepository<Tienda,Long> {
